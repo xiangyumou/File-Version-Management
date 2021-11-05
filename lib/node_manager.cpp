c3a0b3d78d68aa268c6d6f112357898fb92d39e7
Chant Mee <chant_mee@163.com>
2021-11-05 08:51:27 +0800

    完成版本管理器version_manager，版本不太稳定需要进一步测试

--- lib/node_manager.cpp
+++ lib/node_manager.cpp
@@ -1,4 +1,12 @@
-/* Author: MuXiangyu, Chant Mee */
+/**
+   ___ _                 _                      
+  / __| |__   __ _ _ __ | |_    /\/\   ___  ___ 
+ / /  | '_ \ / _` | '_ \| __|  /    \ / _ \/ _ \
+/ /___| | | | (_| | | | | |_  / /\/\ |  __|  __/
+\____/|_| |_|\__,_|_| |_|\__| \/    \/\___|\___|
+
+Author: Mu Xiangyu, Chant Mee 
+*/
 
 #ifndef NODE_MANAGER_CPP
 #define NODE_MANAGER_CPP
--- lib/version_manager.cpp
+++ lib/version_manager.cpp
@@ -1,4 +1,12 @@
-/* Author: MuXiangyu, Chant Mee */
+/**
+   ___ _                 _                      
+  / __| |__   __ _ _ __ | |_    /\/\   ___  ___ 
+ / /  | '_ \ / _` | '_ \| __|  /    \ / _ \/ _ \
+/ /___| | | | (_| | | | | |_  / /\/\ |  __|  __/
+\____/|_| |_|\__,_|_| |_|\__| \/    \/\___|\___|
+
+Author: Mu Xiangyu, Chant Mee 
+*/
 
 #ifndef VERSION_MANAGER_CPP
 #define VERSION_MANAGER_CPP
