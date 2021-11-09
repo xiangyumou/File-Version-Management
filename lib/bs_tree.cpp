3eb10b41e3e4a13e1cc6531416c14274dd04e1d8
Chant Mee <chant_mee@163.com>
2021-11-09 20:12:15 +0800

    完成了部分文件夹扫描功能。完成对bs_tree的注释

--- lib/bs_tree.cpp
+++ lib/bs_tree.cpp
@@ -16,7 +16,18 @@ Author: Mu Xiangyu, Chant Mee
 #include <algorithm>
 #include <vector>
 
+/**
+ * @brief 
+ * This class implements the basic functions of the file system tree structure.
+ * This class only implements the most basic operations on the tree, and other 
+ * specific operations such as file addition, deletion, checking and modification 
+ * are implemented in the FileSystem class.
+ */
 struct treeNode {
+    /**
+     * @brief 
+     * List the types of tree nodes. It may be files, folders, head nodes.
+     */
     enum TYPE {
         FILE = 0, DIR, HEAD_NODE
     };
@@ -35,17 +46,190 @@ private:
     Logger &logger = Logger::get_logger();
     NodeManager &node_manager = NodeManager::get_node_manager();
 protected:
+    /**
+     * @brief 
+     * The path vector plays a very important role in the entire tree and file system. 
+     * This variable holds the simple path from the root node to the node to be operated.
+     * 
+     * When the user needs to enter a folder, first change the content of the path to 
+     * make it point to the target folder, and then add first_son to the path variable
+     * through other functions, and then enter the folder; 
+     * When the user wants to delete the file, It also points to the file through the 
+     * same operation, and then deletes it through other functions; 
+     * There is a very important function in the FileSystem class called rebuild_tree, 
+     * which also uses path to find the node it needs and rebuild nodes. 
+     * And many more.. All in all, this is a very very important variable.
+     */
     std::vector<treeNode*> path;
 
+    /**
+     * @brief
+     * Use this function to check whether the nodes in the path are all legal.
+     * What it checks are:
+     * 1. Whether path is empty. If it is empty, this is abnormal, because there should 
+     * be at least one root directory in the path.
+     * 2. Check whether there is a null pointer in the path.
+     * 
+     * @return true 
+     * No problem with the path.
+     * 
+     * @return false 
+     * The path is empty or there is a null pointer.
+     */
     bool check_path();
+
+    /**
+     * @brief
+     * Use this function to check whether a node in the tree is legal.
+     * What it checks are:
+     * 1. Whether the node is a null pointer.
+     * 2. Check whether the counter of the node is less than or equal to 0. 
+     * If it is less than or equal to 0, then the node should be deleted, but it has 
+     * not been deleted now, which is obviously abnormal.
+     * 
+     * @param p
+     * The node you want to check.
+     * 
+     * @param line 
+     * The line number where you are calling the function. 
+     * This is mainly used to help you locate the wrong location. 
+     * (You can't let the wrong position in this function. QAQ)
+     * 
+     * @return true 
+     * This means that there is no problem with the node.
+     * 
+     * @return false 
+     * This means that the node check failed, and the specific reason can be 
+     * obtained through the information in the logger.
+     */
     bool check_node(treeNode *p, int line);
+
+    /**
+     * @brief 
+     * Check if the last element in path is a child node.
+     * The principle of inspection is to determine whether the node type of the 
+     * tree is head_node.
+     * 
+     * @return true 
+     * This means that the last node in the path is a child node.
+     * 
+     * @return false 
+     * This means that the node is not a child node or the node's node test failed.
+     */
     bool is_son();
+
+    /**
+     * @brief 
+     * Adjust the path to the last node in the folder.
+     * It is by constantly accessing next_brother until it gets the null pointer position.
+     * 
+     * @return true 
+     * The path is successfully adjusted to the last node in the folder.
+     * 
+     * @return false 
+     * The path check before or after adjustment failed.
+     */
     bool goto_tail();
+
+    /**
+     * @brief 
+     * Adjust the path to the head node in the folder.
+     * It is by constantly checking whether the last element in the path is a child node, 
+     * and if it is not, the last element is continuously popped up until the child node 
+     * is found.
+     * 
+     * @return true 
+     * The path is successfully adjusted to the head node.
+     * 
+     * @return false 
+     * The path check before or after adjustment failed.
+     */
     bool goto_head();
+
+    /**
+     * @brief 
+     * Check if the name exists in the current folder.
+     * 
+     * @param name 
+     * The name you want to check.
+     * 
+     * @return true 
+     * The name exists in this folder.
+     * 
+     * @return false 
+     * The name does not exist in the folder or the list_directory_contents 
+     * function returns an error.
+     */
     bool name_exist(std::string name);
+
+    /**
+     * @brief 
+     * Adjust the path to the node named name.
+     * 
+     * @param name 
+     * Adjust the path to the node named name.
+     * 
+     * @return true 
+     * The path is successfully adjusted to the target node.
+     * 
+     * @return false 
+     * The target node does not exist or the goto_head function returns an error.
+     */
     bool go_to(std::string name);
+
+    /**
+     * @brief 
+     * Adjust the path to the upper level directory.
+     * It should be noted that this function will reserve two nodes in 
+     * the path, one is the root directory, and the other is the head node corresponding 
+     * to root's first_son.
+     * 
+     * @return true 
+     * The path is successfully adjusted to the upper level directory.
+     * 
+     * @return false 
+     * The goto_head or check_path function returns an error. 
+     */
     bool goto_last_dir();
+
+    /**
+     * @brief 
+     * List all file folders in the current directory. 
+     * The listed list is arranged in the order of creation. 
+     * The list only contains the names of files or folders, not their types, creation 
+     * time, and modification time. This information can be obtained through the
+     * get_update_time, get_create_time, and get_type functions in the FileSystem class.
+     * 
+     * @param content 
+     * The names of files and folders in this folder are stored in this variable. 
+     * Note that this variable is a reference.
+     * 
+     * @return true 
+     * The names of the files and folders in the folder have been saved to the 
+     * content variable.
+     * 
+     * @return false 
+     * The goto_head function or check_path function returns an error.
+     */
     bool list_directory_contents(std::vector<std::string> &content);
+
+    /**
+     * @brief 
+     * Get the directory of the current folder.
+     * 
+     * @param path 
+     * The path of the current folder is stored in this variable.
+     * The principle of this function is:
+     * Save the original path array first. Then keep goto_head, record the name of 
+     * the penultimate node of the path, and goto_last_dir until the number of nodes 
+     * in the path becomes two. (Only root and root's first_son —— head_node)
+     * 
+     * @return true 
+     * Successfully get the path of the current folder and store the path in the path array.
+     * 
+     * @return false 
+     * The goto_head function or the goto_last_dir function returns an error.
+     */
     bool get_current_path(std::vector<std::string> &path);
 };
 
@@ -53,6 +237,7 @@ protected:
 
                         /* ======= struct treeNode ======= */
 treeNode::treeNode() = default;
+
 treeNode::treeNode(TYPE type) {
     this->type = type;
     this->cnt = 1;
@@ -157,11 +342,11 @@ bool BSTree::list_directory_contents(std::vector<std::string> &content) {
 
 bool BSTree::get_current_path(std::vector<std::string> &p) {
     auto path_backup = path;
-    goto_head();
+    if (!goto_head()) return false;
     while (path.size() > 2) {
         p.push_back(node_manager.get_name(path[path.size() - 2]->link));
-        goto_last_dir();
-        goto_head();
+        if (!goto_last_dir()) return false;
+        if (!goto_head()) return false;
     }
     p.push_back(node_manager.get_name(path.front()->link));
     path = path_backup;
---
+++ lib/dir_scanner.py
@@ -0,0 +1,77 @@
+import os
+
+res = []
+directory = r"/Users/chant/Projects"
+for root, dirs, files in os.walk(directory, topdown=False):
+    for name in dirs:
+        dir_path = os.path.join(root, name)
+        dir_path = dir_path.split("/")
+        dir_path = dir_path[1:-1]
+        if len(res) == 0 or len(dir_path) != len(res[-1]):
+            res.append(dir_path)
+        else:
+            for i in range(len(res[-1])):
+                if res[-1][i] != dir_path[i]:
+                    res.append(dir_path)
+                    break
+
+def escape(s: str):
+   s = s.replace(" ", "\s")
+   s = s.replace("\n", "\\n")
+   s = s.replace("\t", "\\t")
+   s = s.replace("\\", "\\\\")
+   return s
+
+def is_type_valid(dir):
+    valid_type = ["md", "txt", "cpp", "c", "m"]
+    dir = dir.split(".")
+    for vt in valid_type:
+        if dir[-1] == vt:
+            return True
+    return False
+
+def get_content(dir):
+    if is_type_valid(dir) is not True:
+        return ""
+    with open(dir, "r", encoding='ISO-8859-1') as f:
+        content = f.read()
+        f.close()
+        return content
+
+for path in res:
+    for name in path:
+        name = escape(name)
+        print("mkdir " + name)
+        print("cd " + name)
+    for _ in range(len(path)):
+        print("cdl")
+
+# res = []
+# for root, dirs, files in os.walk(directory, topdown=False):
+#     for name in files:
+#         file_path = os.path.join(root, name)
+#         file_path = file_path.split("/")
+#         file_path = file_path[1:]
+#         if len(res) == 0 or len(file_path) != len(res[-1]):
+#             res.append(file_path)
+#         else:
+#             for i in range(len(res[-1])):
+#                 if res[-1][i] != file_path[i]:
+#                     res.append(file_path)
+#                     break
+
+# for path in res:
+#     no_escape_path = "/"
+#     for i in range(len(path)):
+#         no_escape_path = no_escape_path + path[i]
+#         if i < len(path) - 1:
+#             no_escape_path = no_escape_path + "/"
+#         path[i] = escape(path[i])
+#         if i < len(path) - 1:
+#             print("cd " + path[i])
+#         else:
+#             print("touch " + path[i])
+#             print(no_escape_path)
+#             content = get_content(no_escape_path)
+#             content = escape(content)
+#             print("update_content " + path[i] + " " + content)
